d39355a8f0c43c26f6cbd9c45760e88bd8d1a102
Bijay Bista <bijaybista@bijays-air.mynetworksettings.com>
2023-01-18 23:30:56 -0500

    working on developer branch

--- JavaTutorial.java
+++ JavaTutorial.java
@@ -1,5 +1,5 @@
 
-public GitTutorial {
+public JavaTutorial {
     public static void main (String [] args){
         System.out.println("This is Git Tutorial Java and we are inside UAT branch!");
 
